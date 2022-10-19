b2c52a8eb19c6473b0d8e37986fdf3e8fa12e392
williamsusanto <susanti711@yahoo.com>
2022-10-18 22:22:35 -0700

    Added chat messaging code

---
+++ main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: const Text('Flutter is Fun!'),
+          
+        ),
+        body: Center(
+          child: Container(
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "Type message",
+                border: InputBorder.none,
+                
+              ),
+            )
+          ),
+        )
+      )
+    );
+  }
+}
