c1cc1738669bc4c795469647355c90b6de976054
williamsusanto <susanti711@yahoo.com>
2022-10-25 23:06:14 -0700

    Test

--- lib/screens/messages/components/body.dart
+++ lib/screens/messages/components/body.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 
 import '../../../constants.dart';
 import '../../../models/ChatMessage.dart';
-import 'chat_input_field.dart';
 import 'message.dart';
 
 class Body extends StatelessWidget {
+  final _textController = TextEditingController();
+  String message = '';
+  List<Message> messages = [];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -20,7 +23,90 @@ class Body extends StatelessWidget {
             ),
           ),
         ),
-        ChatInputField(),
+        Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: kDefaultPadding,
+        vertical: kDefaultPadding / 2,
+      ),
+      decoration: BoxDecoration(
+        color: Theme.of(context).scaffoldBackgroundColor,
+        boxShadow: [
+          BoxShadow(
+            offset: const Offset(0, 4),
+            blurRadius: 32,
+            color: const Color(0xFF087949).withOpacity(0.08),
+          ),
+        ],
+      ),
+      child: SafeArea(
+        child: Row(
+          children: [
+            const Icon(Icons.mic, color: kPrimaryColor),
+            const SizedBox(width: kDefaultPadding),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: kDefaultPadding * 0.75,
+                ),
+                decoration: BoxDecoration(
+                  color: kPrimaryColor.withOpacity(0.05),
+                  borderRadius: BorderRadius.circular(40),
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.sentiment_satisfied_alt_outlined,
+                      color: Theme.of(context)
+                          .textTheme
+                          .bodyText1!
+                          .color!
+                          .withOpacity(0.64),
+                    ),
+                    const SizedBox(width: kDefaultPadding / 4),
+                     Expanded(
+                      child: TextField(
+                        controller: _textController,
+                        decoration: InputDecoration(
+                          hintText: "Type message",
+                          border: InputBorder.none,
+                        ),
+                        onSubmitted: (_) => setState(() {
+                        if (_textController.text.isNotEmpty) {
+                          message = _textController.text;
+                          messages.insert(
+                              0,
+                              Message(
+                                  text: message, createdAt: DateTime.now()));
+                          _textController.clear();
+                        }
+                      }),
+                      ),
+                    ),
+                    Icon(
+                      Icons.attach_file,
+                      color: Theme.of(context)
+                          .textTheme
+                          .bodyText1!
+                          .color!
+                          .withOpacity(0.64),
+                    ),
+                    const SizedBox(width: kDefaultPadding / 4),
+                    Icon(
+                      Icons.camera_alt_outlined,
+                      color: Theme.of(context)
+                          .textTheme
+                          .bodyText1!
+                          .color!
+                          .withOpacity(0.64),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
       ],
     );
   }
